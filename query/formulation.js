c53a29f2dc28bec5ab07932b854b92bc05dfbb76
luqmaansgithub <luqmaansalie@gmail.com>
2015-09-06 15:40:56 +0200

    Update formulation.js

--- query/formulation.js
+++ query/formulation.js
@@ -3,8 +3,246 @@
 module.exports = {
 
 
-	formulate: function(query, callback){
-		//do stuff
+	function form(query) {
+            var stopwords = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"];
+
+            var terms = query.split(" ");
+
+            arrayLength = terms.length;
+            var result = "";
+            for (var i = 0; i < arrayLength; i++) {
+                if (stopwords.indexOf(terms[i]) == -1) {
+                    result = result + " " + terms[i];
+                }
+            }
+
+            alert(result.trim());
+
+        }
+        
+        //===================================================================================================
+        
+        var stemmer = (function() {
+            var step2list = {
+                "ational": "ate",
+                    "tional": "tion",
+                    "enci": "ence",
+                    "anci": "ance",
+                    "izer": "ize",
+                    "bli": "ble",
+                    "alli": "al",
+                    "entli": "ent",
+                    "eli": "e",
+                    "ousli": "ous",
+                    "ization": "ize",
+                    "ation": "ate",
+                    "ator": "ate",
+                    "alism": "al",
+                    "iveness": "ive",
+                    "fulness": "ful",
+                    "ousness": "ous",
+                    "aliti": "al",
+                    "iviti": "ive",
+                    "biliti": "ble",
+                    "logi": "log"
+            },
+
+            step3list = {
+                "icate": "ic",
+                    "ative": "",
+                    "alize": "al",
+                    "iciti": "ic",
+                    "ical": "ic",
+                    "ful": "",
+                    "ness": ""
+            },
+
+            c = "[^aeiou]", // consonant
+                v = "[aeiouy]", // vowel
+                C = c + "[^aeiouy]*", // consonant sequence
+                V = v + "[aeiou]*", // vowel sequence
+
+                mgr0 = "^(" + C + ")?" + V + C, // [C]VC... is m>0
+                meq1 = "^(" + C + ")?" + V + C + "(" + V + ")?$", // [C]VC[V] is m=1
+                mgr1 = "^(" + C + ")?" + V + C + V + C, // [C]VCVC... is m>1
+                s_v = "^(" + C + ")?" + v; // vowel in stem
+
+            function dummyDebug() {}
+
+            function realDebug() {
+                console.log(Array.prototype.slice.call(arguments).join(' '));
+            }
+
+            return function(w, debug) {
+                var
+                stem,
+                suffix,
+                firstch,
+                re,
+                re2,
+                re3,
+                re4,
+                debugFunction,
+                origword = w;
+
+                if (debug) {
+                    debugFunction = realDebug;
+                } else {
+                    debugFunction = dummyDebug;
+                }
+
+                if (w.length < 3) {
+                    return w;
+                }
+
+                firstch = w.substr(0, 1);
+                if (firstch == "y") {
+                    w = firstch.toUpperCase() + w.substr(1);
+                }
+
+                // Step 1a
+                re = /^(.+?)(ss|i)es$/;
+                re2 = /^(.+?)([^s])s$/;
+
+                if (re.test(w)) {
+                    w = w.replace(re, "$1$2");
+                    debugFunction('1a', re, w);
+
+                } else if (re2.test(w)) {
+                    w = w.replace(re2, "$1$2");
+                    debugFunction('1a', re2, w);
+                }
+
+                // Step 1b
+                re = /^(.+?)eed$/;
+                re2 = /^(.+?)(ed|ing)$/;
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    re = new RegExp(mgr0);
+                    if (re.test(fp[1])) {
+                        re = /.$/;
+                        w = w.replace(re, "");
+                        debugFunction('1b', re, w);
+                    }
+                } else if (re2.test(w)) {
+                    var fp = re2.exec(w);
+                    stem = fp[1];
+                    re2 = new RegExp(s_v);
+                    if (re2.test(stem)) {
+                        w = stem;
+                        debugFunction('1b', re2, w);
+
+                        re2 = /(at|bl|iz)$/;
+                        re3 = new RegExp("([^aeiouylsz])\\1$");
+                        re4 = new RegExp("^" + C + v + "[^aeiouwxy]$");
+
+                        if (re2.test(w)) {
+                            w = w + "e";
+                            debugFunction('1b', re2, w);
+
+                        } else if (re3.test(w)) {
+                            re = /.$/;
+                            w = w.replace(re, "");
+                            debugFunction('1b', re3, w);
+
+                        } else if (re4.test(w)) {
+                            w = w + "e";
+                            debugFunction('1b', re4, w);
+                        }
+                    }
+                }
+
+                // Step 1c
+                re = new RegExp("^(.*" + v + ".*)y$");
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    stem = fp[1];
+                    w = stem + "i";
+                    debugFunction('1c', re, w);
+                }
+
+                // Step 2
+                re = /^(.+?)(ational|tional|enci|anci|izer|bli|alli|entli|eli|ousli|ization|ation|ator|alism|iveness|fulness|ousness|aliti|iviti|biliti|logi)$/;
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    stem = fp[1];
+                    suffix = fp[2];
+                    re = new RegExp(mgr0);
+                    if (re.test(stem)) {
+                        w = stem + step2list[suffix];
+                        debugFunction('2', re, w);
+                    }
+                }
+
+                // Step 3
+                re = /^(.+?)(icate|ative|alize|iciti|ical|ful|ness)$/;
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    stem = fp[1];
+                    suffix = fp[2];
+                    re = new RegExp(mgr0);
+                    if (re.test(stem)) {
+                        w = stem + step3list[suffix];
+                        debugFunction('3', re, w);
+                    }
+                }
+
+                // Step 4
+                re = /^(.+?)(al|ance|ence|er|ic|able|ible|ant|ement|ment|ent|ou|ism|ate|iti|ous|ive|ize)$/;
+                re2 = /^(.+?)(s|t)(ion)$/;
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    stem = fp[1];
+                    re = new RegExp(mgr1);
+                    if (re.test(stem)) {
+                        w = stem;
+                        debugFunction('4', re, w);
+                    }
+                } else if (re2.test(w)) {
+                    var fp = re2.exec(w);
+                    stem = fp[1] + fp[2];
+                    re2 = new RegExp(mgr1);
+                    if (re2.test(stem)) {
+                        w = stem;
+                        debugFunction('4', re2, w);
+                    }
+                }
+
+                // Step 5
+                re = /^(.+?)e$/;
+                if (re.test(w)) {
+                    var fp = re.exec(w);
+                    stem = fp[1];
+                    re = new RegExp(mgr1);
+                    re2 = new RegExp(meq1);
+                    re3 = new RegExp("^" + C + v + "[^aeiouwxy]$");
+                    if (re.test(stem) || (re2.test(stem) && !(re3.test(stem)))) {
+                        w = stem;
+                        debugFunction('5', re, re2, re3, w);
+                    }
+                }
+
+                re = /ll$/;
+                re2 = new RegExp(mgr1);
+                if (re.test(w) && re2.test(w)) {
+                    re = /.$/;
+                    w = w.replace(re, "");
+                    debugFunction('5', re, re2, w);
+                }
+
+                // and turn initial Y back to y
+                if (firstch == "y") {
+                    w = firstch.toLowerCase() + w.substr(1);
+                }
+
+
+                return w;
+            }
+        })();
+        
+        
+        //======================================================================================================
+        
 
 
 
